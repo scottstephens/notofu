1447070ffd7a30c994a2fbcc64c1c77c20ba79b1
Scott Stephens <stephens.js@gmail.com>
2025-08-20 20:47:02 +0000

    Add flake with initial dev environment

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "notofu: a project to make it easier to avoid SSH trust on first use";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            python3
+            uv
+          ];
+
+          shellHook = ''
+            export PS1="[sysadmin shell]$ "
+          '';
+        };
+      });
+}
